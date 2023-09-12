16ae33b258ba56ecaebc69ccab552f07965032b5
Júlia Nascimento <juliavcostan@gmail.com>
2023-09-12 14:52:23 -0300

    Esse código funciona da seguinte forma: De inicio para da inicio ao aprendizado ao tipo (SET). Como no exemplo, é demostrada algumas formas de mostrar ao usuário uma determinada resposta.

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main (){
+
+print('');
+ Set<String> teste = {'Oi', 'Tim', 'Claro', 'Vivo'};  
+ print(teste);
+ //(igual a lista, porem não aceita elementos repetidos, e ao inves de couchetes usa chaves.)
+ 
+ print('');
+}
